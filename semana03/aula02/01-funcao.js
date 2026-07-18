e367bd2acc0f09c3a90e8d519759c6bfb1390144
Priscila Beatriz <priscilabesouza@gmail.com>
2026-07-17 21:14:10 -0300

    Recupera arquivos das semanas 02 e 03

---
+++ semana03/aula02/01-funcao.js
@@ -0,0 +1,16 @@
+/*saudacao("Priscila")
+
+function saudacao(nome) {
+    console.log("Hello, ", nome)
+}
+*/
+// const nomeAleatorio = "Yamal"
+
+// saudacao(nomeAleatorio)
+
+// saudacao()
+
+// saudacao()
+
+// console.log(`Hello, ` nome)
+
---
+++ semana03/aula02/04-arrow.js
@@ -0,0 +1,20 @@
+function somaTradiconal(a, b){
+return a + b;
+}
+
+console.log(somaTradiconal(5, 100));
+
+const soma = (a, b) => a + b;
+
+const subtracao = (a, b) => {
+    const resultadoSubtracao = a - b;
+    return resultadoSubtracao;
+}
+
+console.log(soma(90, 80));
+
+console.log(subtracao(1, 1));
+
+const resultado = soma(10, 20);
+
+console.log(resultado);
